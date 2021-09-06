ef4852103c62d1319a3d38adacf8e034924966de
Akshay G <akshayg676@gmail.com>
2021-09-06 13:16:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//                   Rules
+// - A live cell with less than 2 living neighbours dies.
+// - A live cell with 2 or 3 live neighbours survives.
+// - A live cell with 4+ live neighbours dies.
+// - A dead cell with 3 live neighbours comes back to life.
+
+// called once at the beginning of the program
+function setup() {}
+
+// called repeatedly
+function draw() {}
