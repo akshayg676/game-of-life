37bcab5d8ee72901e021433505802a8da277fc10
Akshay G <akshayg676@gmail.com>
2021-09-07 18:57:59 +0530

    grid transform done

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-//                   Rules
-// - A live cell with less than 2 living neighbours dies.
-// - A live cell with 2 or 3 live neighbours survives.
-// - A live cell with 4+ live neighbours dies.
-// - A dead cell with 3 live neighbours comes back to life.
+/*                  Rules
+- A live cell with less than 2 living neighbours dies.
+- A live cell with 2 or 3 live neighbours survives.
+- A live cell with 4+ live neighbours dies.
+- A dead cell with 3 live neighbour000s comes back to life. */
 
 let rows = 20,
   cols = 20;
@@ -23,11 +23,27 @@ function createGrid() {
 function setup() {
   createCanvas(600, 600); // created a 600*600 canvas
   console.log(grid);
+  grid[10][15] = true;
 }
 
 // called repeatedly
 function draw() {
   background(255); //defines canvas background color
+
+  //display grid
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (grid[i][j]) {
+        fill(0);
+        rect(
+          (j / cols) * width,
+          (i / rows) * height,
+          width / cols,
+          height / rows
+        );
+      }
+    }
+  }
 
   stroke(150); // fro light coloured lines
 
